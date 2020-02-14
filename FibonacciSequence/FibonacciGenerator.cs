748baf3128daff6d8b897d03e9039b7833c5ffff
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-02-14 18:48:58 +0300

    feat: add FibonacciGenerator and tests

---
+++ FibonacciSequence/FibonacciGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace FibonacciSequence
+{
+    public static class FibonacciGenerator
+    {
+        /// <summary>Gets first n elements from the Fibonacci series.</summary>
+        /// <param name="n">The n.</param>
+        /// <returns>Array of Fibonacci numbers.</returns>
+        /// <exception cref="ArgumentException">n is less than 2.</exception>
+        public static BigInteger[] GetFibonacciArray(int n)
+        {
+            if (n < 2)
+            {
+                throw new ArgumentException($"{nameof(n)} cannot be less than 2");
+            }
+
+            BigInteger[] returnArray = new BigInteger[n];
+            returnArray[0] = 0;
+            returnArray[1] = 1;
+            for (int i = 2; i < n; i++)
+            {
+                returnArray[i] = returnArray[i - 2] + returnArray[i - 1];
+            }
+
+            return returnArray;
+        }
+    }
+}
